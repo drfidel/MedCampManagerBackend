d74f4cb4d5b04a292cfaf16ea6588cc4e227207e
Akiyo Fidel d <akiyofidel@gmail.com>
2024-02-10 20:32:43 +0300

    serve via network

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@ source venv/bin/activate
 
 cd medcampsite_be
 
-python manage.py runserver --settings=medcampsite_be.settings.local 5050
+python manage.py runserver --settings=medcampsite_be.settings.local 0.0.0.0:5050
 
 
